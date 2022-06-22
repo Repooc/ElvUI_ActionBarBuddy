61b97f5c1ea8f506ee18b74a4820ca1de1dc8e49
Repooc <tjcooper1982@gmail.com>
2022-06-22 14:54:07 -0400

    update locales

--- ElvUI_ActionBarBuddy/Locales/ptBR.lua
+++ ElvUI_ActionBarBuddy/Locales/ptBR.lua
@@ -1,16 +1,17 @@
 -- Portuguese localization file for ptBR.
 local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI", "ptBR")
 
+L["ABB_CREDITS"] = "Just a want to give credit to The following people for helping me maintain this addon with testing, coding, textures and people who also have helped me through donations. Please note for donation acknowledgments, only the names of people who messaged me or noted in the donation will be listed as you ask (within reason of course)."
+L["Coding"] = true
 L["Curseforge Page"] = true
 L["Development Version"] = true
 L["Download"] = true
-L["ABB_CREDITS"] = "Just a want to give credit to The following people for helping me maintain this addon with testing, coding, textures and people who also have helped me through donations. Please note for donation acknowledgments, only the names of people who messaged me or noted in the donation will be listed as you ask (within reason of course)."
 L["Help Testing Development Versions"] = true
 L["Help"] = true
 L["Link to the latest development version."] = true
 L["Misc Options"] = true
 L["Please press the continue button to go onto the next step."] = true
-L["Coding"] = true
+L["The default behaviour of Inherit Global Fade would display the bars if any of the following are true.  You can remove the triggers that you want to ignore so the bars only appear when the triggers you have checked are true."] = true
 L["Wago Page"] = true
 
 --! Do not translate as they are in ElvUI Locales already
