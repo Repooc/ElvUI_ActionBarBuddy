ab9edd2467b1cf0a0e4f17b9c2288264744eb1b9
Repooc <tjcooper1982@gmail.com>
2024-03-16 15:21:21 -0400

    prep 1.16 - toc & cl

--- ElvUI_ActionBarBuddy/Modules/Changelog/Changelog.lua
+++ ElvUI_ActionBarBuddy/Modules/Changelog/Changelog.lua
@@ -6,7 +6,7 @@ local module = E:NewModule('ABB-Changelog', 'AceEvent-3.0', 'AceTimer-3.0')
 local format, gsub, find = string.format, string.gsub, string.find
 
 local ChangelogTBL = {
-	'v1.16 12/8/2023',
+	'v1.16 3/16/2024',
 		'• Database structure was incorrect from the beginning, updated it to be how it was intended... A simple conversion should port your config over, but just in case, go double check the options and update them as needed.',
 		'• Updated "In Combat" to be 3 options, In Combat (same function as before the update when checked), Not In Combat (will have the exact opposite effect as In Combat), Ignore Combat (same function as before the update when not checked, which does not check for combat state at all)',
 		'• Update toc for classic',
