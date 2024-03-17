24db34403f376294b8e8cf92d45f5c2ee80b745e
Repooc <tjcooper1982@gmail.com>
2024-03-17 13:44:18 -0400

    prep 1.17 - toc & cl

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
-v1.16 3/16/2024
+v1.17 3/16/2024
 
+• Re-release of 1.16 fixing staging issues to Curse and Wago
 • Database structure was incorrect from the beginning, updated it to be how it was intended... A simple conversion should port your config over, but just in case, go double check the options and update them as needed.
 • Updated "In Combat" to be 3 options, In Combat (same function as before the update when checked), Not In Combat (will have the exact opposite effect as In Combat), Ignore Combat (same function as before the update when not checked, which does not check for combat state at all)
 • Update toc for classic
--- ElvUI_ActionBarBuddy/Modules/Changelog/Changelog.lua
+++ ElvUI_ActionBarBuddy/Modules/Changelog/Changelog.lua
@@ -6,6 +6,12 @@ local module = E:NewModule('ABB-Changelog', 'AceEvent-3.0', 'AceTimer-3.0')
 local format, gsub, find = string.format, string.gsub, string.find
 
 local ChangelogTBL = {
+	'v1.17 3/16/2024',
+		'• Re-release of 1.16 fixing staging issues to Curse and Wago',
+		'• Database structure was incorrect from the beginning, updated it to be how it was intended... A simple conversion should port your config over, but just in case, go double check the options and update them as needed.',
+		'• Updated "In Combat" to be 3 options, In Combat (same function as before the update when checked), Not In Combat (will have the exact opposite effect as In Combat), Ignore Combat (same function as before the update when not checked, which does not check for combat state at all)',
+		'• Update toc for classic',
+	' ',
 	'v1.16 3/16/2024',
 		'• Database structure was incorrect from the beginning, updated it to be how it was intended... A simple conversion should port your config over, but just in case, go double check the options and update them as needed.',
 		'• Updated "In Combat" to be 3 options, In Combat (same function as before the update when checked), Not In Combat (will have the exact opposite effect as In Combat), Ignore Combat (same function as before the update when not checked, which does not check for combat state at all)',
